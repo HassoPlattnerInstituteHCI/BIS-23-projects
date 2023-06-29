88e2863c450f91c5adaab67a8e821377c631502c
Philip Maus <philip.maus@web.de>
2023-06-29 19:14:09 +0200

    Finally done with Minigolf Lvl1

--- Assets/Scripts/GolfBallShooting.cs
+++ Assets/Scripts/GolfBallShooting.cs
@@ -1,3 +1,4 @@
+using System;
 using DualPantoFramework;
 using SpeechIO;
 using UnityEngine;
@@ -5,38 +6,57 @@ using UnityEngine;
 public class GolfBallShooting : MonoBehaviour
 {
     public float strength;
-    public GameObject golfBall;
-    private UpperHandle _lowerHandle;
+    private UpperHandle _meHandle;
     private SpeechOut _speechOut;
     private bool _shouldStartPulling;
+    private bool _saidCongratulations;
+    public float maxDistance = 0;
 
     private async void Start()
     {
-        _lowerHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        _meHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
         _speechOut = new SpeechOut();
         
-        await _speechOut.Speak("Here's your golf ball");
-        await _lowerHandle.MoveToPosition(golfBall.transform.position);
+        _speechOut.Speak("Here's your golf ball");
+        await _meHandle.MoveToPosition(gameObject.transform.position);
         await _speechOut.Speak("Try to shoot it forward by pulling the handle back");
         
         _shouldStartPulling = true;
     }
 
+    private async void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Goal")
+        {
+            await _speechOut.Speak("Nice shot!");
+        }
+    }
+
     private void FixedUpdate()
     {
-        Vector3 handlePosition = _lowerHandle.GetPosition();
-        if (_shouldStartPulling && handlePosition.z < golfBall.transform.position.z)
+        if (!_shouldStartPulling) return;
+        
+        Vector3 handlePosition = _meHandle.GetPosition();
+        if (handlePosition.z < gameObject.transform.position.z)
         {
-            float distance = Vector3.Distance(handlePosition, golfBall.transform.position);
+            float distance = Vector3.Distance(handlePosition, gameObject.transform.position);
             if (distance < 0.5)
             {
-                _lowerHandle.ApplyForce(Vector3.zero, 0);                
+                _meHandle.StopApplyingForce();
             }
             else
             {
-                Vector3 direction = (golfBall.transform.position - handlePosition).normalized;
-                _lowerHandle.ApplyForce(direction, strength * distance);
-                Debug.Log("Move Direction: " + direction + " with force: " + (strength * distance));
+                Vector3 direction = (gameObject.transform.position - handlePosition).normalized;
+                _meHandle.ApplyForce(direction, strength * distance);
+                if (distance > maxDistance) maxDistance = distance;
+            }
+        } else if (maxDistance > 0.5)
+        {
+            gameObject.transform.position = new Vector3(handlePosition.x, 1.5f, handlePosition.z);;
+            if (gameObject.transform.position.z < 2.5 && !_saidCongratulations)
+            {
+                _saidCongratulations = true;
+                _speechOut.Speak("Nice shot!");
             }
         }
     }
