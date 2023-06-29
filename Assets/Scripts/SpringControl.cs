981908276c97af8e05a29e7d85000c3a5fa2755f
Iven Schlegelmilch <iven.schlegelmilch@googlemail.com>
2023-06-29 21:28:16 +0200

    Lvl. 1 DualShoot

--- Assets/Scripts/SpringControl.cs
+++ Assets/Scripts/SpringControl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using DualPantoFramework;
 using System.IO;
+using System;
+using static UnityEngine.GraphicsBuffer;
 
 public class SpringControl : MonoBehaviour
 {
@@ -10,18 +12,50 @@ public class SpringControl : MonoBehaviour
     PantoHandle handle;
     Vector3 spawnPoint;
     public float forceValue = 1;
+    bool shotBall = false;
 
     void Start()
     {
-        spawnPoint = new Vector3(0, 0, 0);
-        transform.position = spawnPoint;
         handle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        spawnPoint = new Vector3(0, 0, -5);
+        transform.position = spawnPoint;
+        handle.SetPositions(spawnPoint, handle.GetRotation(), spawnPoint);
+        
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        handle.ApplyForce((spawnPoint - transform.position).normalized, (spawnPoint - transform.position).magnitude * forceValue);
+        Vector3 currentForce = spawnPoint - transform.position;
+
+        if(!shotBall)
+        {
+            handle.ApplyForce(currentForce.normalized, currentForce.magnitude * (float)Math.Pow(forceValue, 2.0));
+            if (Math.Abs(handle.GetRotation()) >= 90)
+            {
+                print("shoot");
+                shotBall = true;
+                GetComponent<MeHandle>().enabled = false;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Rigidbody>().AddForce(currentForce*(float)Math.Pow(forceValue, 2.0), ForceMode.Impulse);
+                print(currentForce);
+            }
+
+        }
+        else
+        {
+            handle.SetPositions(transform.position, handle.GetRotation(), spawnPoint);
+        }
+
+
+    }
+
+    void OnDrawGizmos()
+    {
+        
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(spawnPoint, transform.position);
+        
     }
 }
