08951bc539103f9f4b7d55df6495f71f7ef5fff7
Hendrik Higl <hendrik.higl@gmx.de>
2023-07-01 18:02:00 +0200

    Fruit ninja done

---
+++ Assets/ExampleScripts/IntroductionManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using SpeechIO;
+using DualPantoFramework;
+
+public class IntroductionManager : MonoBehaviour
+{
+    SpeechOut speech;
+    public SpawnManager spawnManager;
+
+    async void Start()
+    {
+        speech = new SpeechOut();
+        Level level = GameObject.Find("Panto").GetComponent<Level>();
+        await level.PlayIntroduction();
+        await speech.Speak("This is the fruit, now slice it!");
+        spawnManager.startGame();
+    }
+
+    void OnApplicationQuit()
+    {
+        speech.Stop();
+    }
+
+}
