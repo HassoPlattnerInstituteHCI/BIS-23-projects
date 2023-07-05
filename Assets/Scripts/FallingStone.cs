fd524b91ee6eb8d7a16a81b3dfaa8e76a7d66dfc
nu444 <nils1@posteo.de>
2023-07-05 14:47:06 +0200

    einfaches Spiel

--- Assets/Scripts/FallingStone.cs
+++ Assets/Scripts/FallingStone.cs
@@ -3,13 +3,16 @@ using DualPantoFramework;
 
 public class FallingStone : MonoBehaviour
 {
+    public GameObject copyPrefab;
     bool free = true;
     PantoHandle upperHandle;
     bool isswitched = false;
-    void Start()
+    int[] heightmap = {3, 1, 0, 2, 4};
+    async void Start()
     {
         upperHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
-        InvokeRepeating("FallDown", 2.0f, 2f);
+        InvokeRepeating("FallDown", 2.0f, 0.5f);
+        //await upperHandle.SwitchTo(gameObject, 20f);
     }
 
     async void FixedUpdate()
@@ -19,8 +22,11 @@ public class FallingStone : MonoBehaviour
         var distance = transform.position.x - upperHandle.HandlePosition(transform.position).x;
         Debug.Log(distance);
 
+
         if (Mathf.Abs(distance) > 0.5)
         {
+            
+
             if (distance < 0 && transform.position.x < 2)
             {
                 transform.position += new Vector3(1, 0, 0);
@@ -29,12 +35,26 @@ public class FallingStone : MonoBehaviour
             {
                 transform.position -= new Vector3(1, 0, 0);
             }
+            
         }
+        // if(Mathf.Abs(distance) > 0.2){
+        //     upperHandle.MoveToPosition(transform.position, 100f, true);
+        // }
     }
 
     void FallDown()
     {
-        transform.position -= new Vector3(0, 0, 1);
+        int xPos = (int) transform.position.x + 2;
+        if(transform.position.z > (-14 + heightmap[xPos])){
+            transform.position -= new Vector3(0, 0, 1);
+        }else{
+            GameObject fixedCube = Instantiate(copyPrefab, transform.position, copyPrefab.transform.rotation);
+            PantoBoxCollider collider = fixedCube.AddComponent<PantoBoxCollider>();
+            collider.CreateObstacle();
+            collider.Enable();
+            transform.position = new Vector3(0, 0, -3);
+            heightmap[xPos]++;
+        }
     }
 
     void Update()
