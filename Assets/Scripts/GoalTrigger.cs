06033cedd9c988cae6ab71eee4593d382087b25e
Philip Maus <philip.maus@web.de>
2023-07-06 20:02:03 +0200

    Goal Notification in Explore Mode

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,39 @@
+using System;
+using DualPantoFramework;
+using SpeechIO;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+
+    // Components of Panto
+    private LowerHandle _lowerHandle;
+    private readonly String _meHandleGodObject = "ItHandleGodObject";
+
+    public int vibrationIntensity = 15;
+    private int _vibrationMultiplicationFactor = -1;
+
+    private void Start()
+    {
+        _lowerHandle = GameObject.Find("Panto").GetComponent<LowerHandle>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Me Handle is on Object and we're in Explore Mode: Say Goal to Identify Goal 
+        if (other.gameObject.name == _meHandleGodObject && MiniGolfV2.currentGameMode == MiniGolfV2.GameModes.ExploreMode)
+        { 
+            MiniGolfV2.speechOut.Speak("Goal");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // If Me Handle is on Object and we're in Explore Mode: Vibrate It Handle for more haptics user 
+        if (other.gameObject.name == _meHandleGodObject && MiniGolfV2.currentGameMode == MiniGolfV2.GameModes.ExploreMode)
+        {
+            _lowerHandle.Rotate(_lowerHandle.GetRotation() + vibrationIntensity * _vibrationMultiplicationFactor);
+            _vibrationMultiplicationFactor *= -1;
+        }
+    }
+}
