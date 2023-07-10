df8d6d52c4f73e627c88097f4fe3bb2b4ed969b6
Deltapie-dev <64013655+Deltapie-dev@users.noreply.github.com>
2023-07-10 10:00:38 +0200

    changed mat directory

--- Assets/Scripts/Lvl1/Intro.cs
+++ Assets/Scripts/Lvl1/Intro.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SpeechIO;
 
 public class ShooterIntro : MonoBehaviour
 {
+    private SpeechOut sp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sp = new SpeechOut();
+        sp.Speak("Pick up the gun");
+        //sp.Speak("The closer you are the louder the sound");
     }
 
     // Update is called once per frame
