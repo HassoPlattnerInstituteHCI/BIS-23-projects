149de7987b42dad8e2a1b9b1ac5f1892aa929e06
Deltapie-dev <64013655+Deltapie-dev@users.noreply.github.com>
2023-07-13 17:40:11 +0200

    enemy working

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,50 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks;
-using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 6f;
-    public GameObject player;
-    private Rigidbody enemyRb;
-    
-    private bool enemyActive = false;
-    
-    public string displayName;
-    public AudioClip nameClip;
+    private GameObject player;
+    public float speed;
 
-
+    // Start is called before the first frame update
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
-        
+        player = GameObject.Find("PlayerUpper");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -2)
-        {
-            //GameObject.FindObjectOfType<SpawnManager>().FindOtherEnemy();
-            Destroy(gameObject);
-        }
-    }
-    
-    // public async Task ActivateEnemy()
-    // {
-    //     enemyActive = true;
-    // }
-
-
-    void FixedUpdate()
-    {
-            /// challenge: set lookDirection to "enemy to player" vector
-            Vector3 lookDirection = player.transform.position - transform.position;
-            //transform.position += lookDirection.normalized * speed * Time.fixedDeltaTime;
-           enemyRb.AddForce(lookDirection.normalized * speed * Time.fixedDeltaTime, ForceMode.Impulse);
-     
-    }
-
-    void MoveToPlayer() {
         
+    }
+
+    void FixedUpdate() 
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
